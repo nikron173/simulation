4e3c776231641c138b11be0736336e1ca4836550
nikron173 <nikron17355@gmail.com>
2023-07-12 23:06:28 +0400

    Изменен класс Grass

--- src/com/nikron/simulation/piece/Entity.java
+++ src/com/nikron/simulation/piece/Entity.java
@@ -18,9 +18,9 @@ public abstract class Entity {
     }
 
     //находит по координатам x и y объект entity, если такого нет, то возвращает null
-    public static Entity findMapEntity(List<Entity> entityList, int x, int y){
-        if (entityList == null) return null;
-        return entityList.stream().filter(obj -> obj.getX() == x && obj.getY()==y).findFirst().orElse(null);
-    }
+//    public static Entity findMapEntity(List<Entity> entityList, int x, int y){
+//        if (entityList == null) return null;
+//        return entityList.stream().filter(obj -> obj.getX() == x && obj.getY()==y).findFirst().orElse(null);
+//    }
 
 }
--- src/com/nikron/simulation/piece/Grass.java
+++ src/com/nikron/simulation/piece/Grass.java
@@ -2,8 +2,12 @@ package com.nikron.simulation.piece;
 
 public class Grass extends Entity {
 
-    public Grass(int x, int y) {
-        super(x, y);
+    public Grass(Coordinates coordinates) {
+        super(coordinates);
     }
 
+    @Override
+    public String toString() {
+        return "\uD83C\uDF40";
+    }
 }
